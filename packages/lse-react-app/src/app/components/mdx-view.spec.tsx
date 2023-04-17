8a83c24d1f17c587a7991c197936d7bdb3860a4b
aartic88 <aartic88@gmail.com>
2023-04-18 03:07:55 +0530

    Learning test for compiling and rendering mdx on browser
    Learning test for compiling and rendering mdx on browser v2
    
    testing-library/jest-dom

---
+++ packages/lse-react-app/src/app/components/mdx-view.spec.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote } from 'next-mdx-remote';
+
+describe('MdxView', () => {
+  it('learning test next-mdx-remoted compile and render mdx on browser successfully', async () => {
+    const mdxSource = await serialize(`
+    # Testing MDX
+    **bold text**
+    *italicized text*
+    `);
+
+    // console.log(String(mdxSource.compiledSource))
+    const { baseElement } = render(
+      <MDXRemote {...mdxSource} />
+    );
+    expect(baseElement).toBeTruthy();
+    //expect(screen.getByRole('heading')).toHaveTextContent('Testing MDX');
+    //expect(baseElement.querySelector('strong')).toHaveTextContent('bold text');
+    //expect(baseElement.querySelector('em')).toHaveTextContent('italicized text');
+    // screen.debug();
+  });
+});
